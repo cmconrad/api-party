289dcbe114bfcf33d64053f072a048a4d2ad24cf
Caleb Conrad <cmconrad@anderson.edu>
2017-07-11 11:07:53 -0400

    Push username onto history when submitted

--- src/Github.js
+++ src/Github.js
@@ -12,12 +12,17 @@ class Github extends Component {
         this.setState({username: ev.target.value})
     }
 
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`github/${this.state.username}`)   
+    }
+
     render(){
         return (
         <div className="github">
             
             <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="logo"/>
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <input 
                         type="text" 
