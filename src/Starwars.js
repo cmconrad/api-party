df8c2bb2477f7007ad382590eb69d94c58b54749
Caleb Conrad <cmconrad@anderson.edu>
2017-07-11 14:06:45 -0400

    Made a Star Wars API form.

--- src/Starwars.js
+++ src/Starwars.js
@@ -4,7 +4,24 @@ import './Starwars.css'
 
 class Starwars extends Component{
     render(){
-        return <p>Star wars stuff</p>
+        return (
+        <div className="starwars">
+            
+            <img className="starwars-logo" src="https://vignette2.wikia.nocookie.net/starwars/images/4/42/StarWarsOpeningLogo.svg/revision/latest?cb=20120211213511" alt="logo"/>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <input 
+                        type="text" 
+                    />
+                </div>
+                <div>
+                    <button>Look up a Star Wars entity.</button>
+                </div>
+            </form>    
+            <Route exact path="/starwars" render={() => <h3>Please enter a valid Star Wars person, planet, or starship.</h3>}/>
+            <Route path="/starwars/:username" />  
+        </div>
+        )
     }
 }
 
